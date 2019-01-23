88182a123f32de3ed49144504072f68a7c6ef13e
thebioengineer <ellishughes@live.com>
2019-01-22 22:47:50 -0800

    trying to clean things up

--- R/socketCommunication.R
+++ R/socketCommunication.R
@@ -2,16 +2,6 @@
 #' Get code to evaluate from original R Session
 #' @param sandboxCon socket connection to original R session
 receiveSand<-function(sandboxCon){
-  # primer <- recieveMessage(sandboxCon)
-  # ss <- recieveMessage(sandboxCon,maxlen = as.character(primer))
-  # stringToObject(ss)
-  # repeat({
-  #   output<-try(stringToObject(unserialize(sandboxCon)),silent=TRUE)
-  #   if(!inherits(output,"try-error")){
-  #     break
-  #   }
-  # })
-  # output
   unserialize(sandboxCon)
 }
 
@@ -20,9 +10,5 @@ receiveSand<-function(sandboxCon){
 #' @param expr the expression to be sent to the r session on the other end of the `sandboxCon`
 #' @param sandboxCon socket connection to original R session
 sendSand<-function(expr,sandboxCon){
-  # serObject<-objectToString(expr)
-  # sendMessage(sandboxCon,as.character(stri_numbytes(serObject)))
-  # sendMessage(sandboxCon,serObject,maxlen = stri_numbytes(serObject))
-  # serialize(objectToString(expr),sandboxCon)
   serialize(expr,sandboxCon)
 }
