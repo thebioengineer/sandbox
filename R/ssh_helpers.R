dea6307e415a0b5dd3b63a06ff4e09ea5c700934
thebioengineer <ellishughes@live.com>
2021-08-01 16:30:39 -0700

    better escaping

--- R/ssh_helpers.R
+++ R/ssh_helpers.R
@@ -21,33 +21,37 @@ ssh_sandbox_exec <- function(session, expression, rscript_exe = "RScript") {
   
   res <- ssh_exec_internal(session, cmd)
   
-  unserialize(jsonlite::base64_dec(std_out(res)))
+  dexpressionize(std_out(res))
 }
 
 expressionize <- function(x){
-  gsub("\n","\\\\n",jsonlite::base64_enc(serialize(x,connection = NULL)))
+  char_x <- gsub("\n","\\\\n",jsonlite::base64_enc(serialize(x,connection = NULL)))
+  char_x <- gsub("\\","%%",char_x, fixed = TRUE)
+  scan(text = char_x, what = "character", allowEscapes = FALSE,quiet = TRUE,encoding = "utf8")
 }
 
 dexpressionize <- function(x){
-  unserialize(jsonlite::base64_dec(gsub("\\n","\n",paste(x,collapse = ""), fixed=TRUE)))
+  char_x <-scan(text = paste(x,collapse = ""), what = "character", allowEscapes = TRUE,quiet = TRUE)
+  char_x <- gsub("%%","\\",char_x, fixed = TRUE)
+  unserialize(jsonlite::base64_dec(gsub("\\n","\n",char_x, fixed=TRUE)))
 }
+
+
 
 ssh_install_sandbox <- function(session,ref = NULL, force = FALSE){
   
   ssh_rscript_exec(
     session,
-    paste0(
+    shQuote(paste0(
       paste0("sandbox <- inherits(try(find.package(\"sandbox\")),\"try-error\") | ", force,";"),
       "remotes <- inherits(try(find.package(\"remotes\")),\"try-error\");",
       "rv <- R.version;",
       "rdate <- paste0(rv$year,'-',rv$month,'-',rv$day);",
-      "if(remotes){",
-      "install.packages('remotes',repos = paste0('https://mran.microsoft.com/snapshot/',rdate))",
       "};",
       "if(sandbox){",
       paste0("remotes::install_github('thebioengineer/sandbox",ifelse(!is.null(ref),paste0("@",ref),""),"')"),
       "};"
-    ))
+    )))
 }
 
 ssh_installed_packages <- function(session, packages){
@@ -62,6 +66,8 @@ ssh_installed_packages <- function(session, packages){
   )
   
   remote_installed_packages <- unlist(strsplit(std_out(res),"\\s+"))
+  
+  
   
   list(
     remote_installed = remote_installed_packages,
